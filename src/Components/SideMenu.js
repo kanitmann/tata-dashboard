4a0bce25c426a99ef50426e6198c7713fe34fc2d
Kanit Mann <kanitmann90@gmail.com>
2021-08-17 18:26:40 +0530

    Search bar added

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -2,12 +2,25 @@ import React from "react";
 import logo from "../Assets/logo.png";
 
 const SideMenu = (props) => {
-    return <div className="side-menu">
+    return (<div className="side-menu inactive">
         <div className="top-section">
             <div className="logo">
-                <img scr={logo} alt="Tata-logo" />
+                <img src={logo} alt="Tata-logo" />
+            </div>
+            <div className="toggle-menu-btn">
+                <i class="bi bi-arrow-left-square-fill"></i>
+            </div>
+            <div className="search-controller">
+                <button className="search-btn">
+                    <i class="bi bi-search"></i>
+                </button>
+                <input type="text" placeholder="Search" />
+            </div>
+            <div className="divider">
+
             </div>
         </div>
     </div>
-}
+    );
+}   
 export default SideMenu;
